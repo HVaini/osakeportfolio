793146464f74ae225eb6168a48e9289605cb2422
root <root@DESKTOP-D05A159.>
2024-11-30 12:16:00 +0200

    Schema updated

--- schema.sql
+++ schema.sql
@@ -1,9 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 16.4 (Ubuntu 16.4-0ubuntu0.24.04.2)
--- Dumped by pg_dump version 16.4 (Ubuntu 16.4-0ubuntu0.24.04.2)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,20 +15,12 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
--- Name: alembic_version; Type: TABLE; Schema: public; Owner: root
---
 
 CREATE TABLE public.alembic_version (
     version_num character varying(32) NOT NULL
 );
 
 
-ALTER TABLE public.alembic_version OWNER TO root;
-
---
--- Name: portfolio_stocks; Type: TABLE; Schema: public; Owner: root
---
 
 CREATE TABLE public.portfolio_stocks (
     portfolio_id integer NOT NULL,
@@ -42,11 +29,6 @@ CREATE TABLE public.portfolio_stocks (
 );
 
 
-ALTER TABLE public.portfolio_stocks OWNER TO root;
-
---
--- Name: portfolios; Type: TABLE; Schema: public; Owner: root
---
 
 CREATE TABLE public.portfolios (
     id integer NOT NULL,
@@ -57,11 +39,6 @@ CREATE TABLE public.portfolios (
 );
 
 
-ALTER TABLE public.portfolios OWNER TO root;
-
---
--- Name: portfolios_id_seq; Type: SEQUENCE; Schema: public; Owner: root
---
 
 CREATE SEQUENCE public.portfolios_id_seq
     AS integer
@@ -72,18 +49,35 @@ CREATE SEQUENCE public.portfolios_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.portfolios_id_seq OWNER TO root;
-
---
--- Name: portfolios_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
---
 
 ALTER SEQUENCE public.portfolios_id_seq OWNED BY public.portfolios.id;
 
 
---
--- Name: stocks; Type: TABLE; Schema: public; Owner: root
---
+
+CREATE TABLE public.stock_transactions (
+    id integer NOT NULL,
+    portfolio_id integer,
+    stock_id integer,
+    quantity integer NOT NULL,
+    purchase_price numeric(10,2) NOT NULL,
+    purchase_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE SEQUENCE public.stock_transactions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+ALTER SEQUENCE public.stock_transactions_id_seq OWNED BY public.stock_transactions.id;
+
+
 
 CREATE TABLE public.stocks (
     id integer NOT NULL,
@@ -95,11 +89,6 @@ CREATE TABLE public.stocks (
 );
 
 
-ALTER TABLE public.stocks OWNER TO root;
-
---
--- Name: stocks_id_seq; Type: SEQUENCE; Schema: public; Owner: root
---
 
 CREATE SEQUENCE public.stocks_id_seq
     AS integer
@@ -110,18 +99,39 @@ CREATE SEQUENCE public.stocks_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.stocks_id_seq OWNER TO root;
-
---
--- Name: stocks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
---
 
 ALTER SEQUENCE public.stocks_id_seq OWNED BY public.stocks.id;
 
 
---
--- Name: user; Type: TABLE; Schema: public; Owner: root
---
+
+CREATE TABLE public.transactions (
+    id integer NOT NULL,
+    portfolio_id integer NOT NULL,
+    stock_id integer NOT NULL,
+    transaction_type character varying(10),
+    quantity numeric,
+    price_per_share numeric,
+    transaction_date timestamp without time zone DEFAULT now(),
+    CONSTRAINT transactions_price_per_share_check CHECK ((price_per_share > (0)::numeric)),
+    CONSTRAINT transactions_quantity_check CHECK ((quantity > (0)::numeric)),
+    CONSTRAINT transactions_transaction_type_check CHECK (((transaction_type)::text = ANY ((ARRAY['buy'::character varying, 'sell'::character varying])::text[])))
+);
+
+
+
+CREATE SEQUENCE public.transactions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
+ALTER SEQUENCE public.transactions_id_seq OWNED BY public.transactions.id;
+
+
 
 CREATE TABLE public."user" (
     id integer NOT NULL,
@@ -130,11 +140,6 @@ CREATE TABLE public."user" (
 );
 
 
-ALTER TABLE public."user" OWNER TO root;
-
---
--- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: root
---
 
 CREATE SEQUENCE public.user_id_seq
     AS integer
@@ -145,18 +150,10 @@ CREATE SEQUENCE public.user_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.user_id_seq OWNER TO root;
-
---
--- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
---
 
 ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 
 
---
--- Name: users; Type: TABLE; Schema: public; Owner: root
---
 
 CREATE TABLE public.users (
     id integer NOT NULL,
@@ -165,11 +162,6 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO root;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: root
---
 
 CREATE SEQUENCE public.users_id_seq
     AS integer
@@ -180,148 +172,127 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.users_id_seq OWNER TO root;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
---
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
---
--- Name: portfolios id; Type: DEFAULT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolios ALTER COLUMN id SET DEFAULT nextval('public.portfolios_id_seq'::regclass);
 
 
---
--- Name: stocks id; Type: DEFAULT; Schema: public; Owner: root
---
+
+ALTER TABLE ONLY public.stock_transactions ALTER COLUMN id SET DEFAULT nextval('public.stock_transactions_id_seq'::regclass);
+
+
 
 ALTER TABLE ONLY public.stocks ALTER COLUMN id SET DEFAULT nextval('public.stocks_id_seq'::regclass);
 
 
---
--- Name: user id; Type: DEFAULT; Schema: public; Owner: root
---
+
+ALTER TABLE ONLY public.transactions ALTER COLUMN id SET DEFAULT nextval('public.transactions_id_seq'::regclass);
+
+
 
 ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
 
 
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
---
--- Name: alembic_version alembic_version_pkc; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.alembic_version
     ADD CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num);
 
 
---
--- Name: portfolio_stocks portfolio_stocks_pkey; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolio_stocks
     ADD CONSTRAINT portfolio_stocks_pkey PRIMARY KEY (portfolio_id, stock_id);
 
 
---
--- Name: portfolios portfolios_pkey; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolios
     ADD CONSTRAINT portfolios_pkey PRIMARY KEY (id);
 
 
---
--- Name: stocks stocks_pkey; Type: CONSTRAINT; Schema: public; Owner: root
---
+
+ALTER TABLE ONLY public.stock_transactions
+    ADD CONSTRAINT stock_transactions_pkey PRIMARY KEY (id);
+
+
 
 ALTER TABLE ONLY public.stocks
     ADD CONSTRAINT stocks_pkey PRIMARY KEY (id);
 
 
---
--- Name: stocks stocks_symbol_key; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.stocks
     ADD CONSTRAINT stocks_symbol_key UNIQUE (symbol);
 
 
---
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: root
---
+
+ALTER TABLE ONLY public.transactions
+    ADD CONSTRAINT transactions_pkey PRIMARY KEY (id);
+
+
 
 ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
---
--- Name: user user_username_key; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_username_key UNIQUE (username);
 
 
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
---
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_username_key UNIQUE (username);
 
 
---
--- Name: portfolio_stocks portfolio_stocks_portfolio_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolio_stocks
     ADD CONSTRAINT portfolio_stocks_portfolio_id_fkey FOREIGN KEY (portfolio_id) REFERENCES public.portfolios(id) ON DELETE CASCADE;
 
 
---
--- Name: portfolio_stocks portfolio_stocks_stock_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolio_stocks
     ADD CONSTRAINT portfolio_stocks_stock_id_fkey FOREIGN KEY (stock_id) REFERENCES public.stocks(id) ON DELETE CASCADE;
 
 
---
--- Name: portfolios portfolios_stock_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolios
     ADD CONSTRAINT portfolios_stock_id_fkey FOREIGN KEY (stock_id) REFERENCES public.stocks(id);
 
 
---
--- Name: portfolios portfolios_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: root
---
 
 ALTER TABLE ONLY public.portfolios
     ADD CONSTRAINT portfolios_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
---
--- PostgreSQL database dump complete
---
+
+ALTER TABLE ONLY public.stock_transactions
+    ADD CONSTRAINT stock_transactions_portfolio_id_fkey FOREIGN KEY (portfolio_id) REFERENCES public.portfolios(id) ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY public.stock_transactions
+    ADD CONSTRAINT stock_transactions_stock_id_fkey FOREIGN KEY (stock_id) REFERENCES public.stocks(id) ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY public.transactions
+    ADD CONSTRAINT transactions_portfolio_id_fkey FOREIGN KEY (portfolio_id) REFERENCES public.portfolios(id);
+
+
+
+ALTER TABLE ONLY public.transactions
+    ADD CONSTRAINT transactions_stock_id_fkey FOREIGN KEY (stock_id) REFERENCES public.stocks(id);
+
+
 
